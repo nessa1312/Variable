ccd78dab437478602320e4832b110934eecafa18
vanessa <vanesska_denis2011@mail.ru>
2022-09-20 22:36:30 +0300

    Variable Homework2

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,49 @@ public class Main {
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+
+        // Задача 4
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        // Задача 5
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        // Задача 6
+        System.out.println("Задача 6");
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        System.out.println("Общий вес двух бойцов: "+ (boxer1Weight + boxer2Weight));
+        System.out.println("Разница весов бойцов: "+ (boxer2Weight - boxer1Weight));
+
+        // Задача 7
+        System.out.println("Задача 7");
+        System.out.println("Разница весов бойцов (первый способ): "+ (boxer2Weight - boxer1Weight));
+        System.out.println("Разница весов бойцов (второй способ): "+ (boxer2Weight % boxer1Weight));
+
+        // Задача 8
+        System.out.println("Задача 8");
+        var totalWorkingHours = 640;
+        var hoursPerDay = 8;
+        var totalImployees = totalWorkingHours / hoursPerDay;
+        System.out.println("Всего работников в компании – "+ totalImployees +" человек");
+
+        totalImployees = totalImployees + 94;
+        totalWorkingHours = totalImployees * hoursPerDay;
+        System.out.println("Если в компании работает "+ totalImployees +" человека, то всего "+ totalWorkingHours +" часа работы может быть поделено между сотрудниками");
+
     }
 }
